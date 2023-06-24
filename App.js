2a0dc5b27a1bbb7a52e76c42ff1e916c8681d936
Do Loi <maccto@MacBook-Pro-cua-maccto.local>
2023-06-24 18:52:43 +0700

    add package screen

--- App.js
+++ App.js
@@ -1,14 +1,60 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import {Provider} from 'react-redux';
 import store from './App/Redux/store';
+import {Button} from 'react-native';
+
+const Stack = createStackNavigator();
+
+const HomeScreen = ({navigation}) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello, React Native!</Text>
+      <Button
+        title="Go to Login"
+        onPress={() => navigation.navigate('Login')}
+      />
+    </View>
+  );
+};
+
+const LoginScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Login Screen</Text>
+    </View>
+  );
+};
+
+const SignInScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Sign In Screen</Text>
+    </View>
+  );
+};
+
+const SignUpScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Sign Up Screen</Text>
+    </View>
+  );
+};
 
 const App = () => {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text style={styles.text}>Hello, React Native!</Text>
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="SignIn" component={SignInScreen} />
+          <Stack.Screen name="SignUp" component={SignUpScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 };
